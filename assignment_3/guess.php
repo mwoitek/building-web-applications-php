1bb2455c93f8ec91b49e65bd09f4ab833b391bef
Marcio Woitek <woitek@usp.br>
2020-08-24 21:40:52 -0300

    Final version of guess.php

--- assignment_3/guess.php
+++ assignment_3/guess.php
@@ -7,17 +7,17 @@
 <p>
 <?php
     if (!isset($_GET['guess'])) {
-        echo("Missing guess parameter");
+        echo 'Missing guess parameter';
     } else if (strlen($_GET['guess']) < 1) {
-        echo("Your guess is too short");
+        echo 'Your guess is too short';
     } else if (!is_numeric($_GET['guess'])) {
-        echo("Your guess is not a number");
+        echo 'Your guess is not a number';
     } else if ($_GET['guess'] < 57) {
-        echo("Your guess is too low");
+        echo 'Your guess is too low';
     } else if ($_GET['guess'] > 57) {
-        echo("Your guess is too high");
+        echo 'Your guess is too high';
     } else {
-        echo("Congratulations - You are right");
+        echo 'Congratulations - You are right';
     }
 ?>
 </p>
